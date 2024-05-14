6c291a4902f7ec1e1bf270507a3622473c0e068d
scottpham <scotttp1@uci.edu>
2024-05-14 01:36:11 -0700

    .

--- src/main/java/UpdateSecurePassword.java
+++ src/main/java/UpdateSecurePassword.java
@@ -10,15 +10,15 @@ import org.jasypt.util.password.StrongPasswordEncryptor;
 public class UpdateSecurePassword {
 
     /*
-     * 
+     *
      * This program updates your existing moviedb customers table to change the
      * plain text passwords to encrypted passwords.
-     * 
+     *
      * You should only run this program **once**, because this program uses the
      * existing passwords as real passwords, then replace them. If you run it more
      * than once, it will treat the encrypted passwords as real passwords and
      * generate wrong values.
-     * 
+     *
      */
     public static void main(String[] args) throws Exception {
 
@@ -31,16 +31,16 @@ public class UpdateSecurePassword {
         Statement statement = connection.createStatement();
 
         // change the customers table password column from VARCHAR(20) to VARCHAR(128)
-        String alterQuery = "ALTER TABLE customers MODIFY COLUMN password VARCHAR(128)";
+        String alterQuery = "ALTER TABLE employees MODIFY COLUMN password VARCHAR(128)";
         int alterResult = statement.executeUpdate(alterQuery);
-        System.out.println("altering customers table schema completed, " + alterResult + " rows affected");
+        System.out.println("altering employees table schema completed, " + alterResult + " rows affected");
 
         // get the ID and password for each customer
-        String query = "SELECT id, password from customers";
+        String query = "SELECT email, password from employees;";
 
         ResultSet rs = statement.executeQuery(query);
 
-        // we use the StrongPasswordEncryptor from jasypt library (Java Simplified Encryption) 
+        // we use the StrongPasswordEncryptor from jasypt library (Java Simplified Encryption)
         //  it internally use SHA-256 algorithm and 10,000 iterations to calculate the encrypted password
         PasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
 
@@ -49,15 +49,16 @@ public class UpdateSecurePassword {
         System.out.println("encrypting password (this might take a while)");
         while (rs.next()) {
             // get the ID and plain text password from current table
-            String id = rs.getString("id");
+            String id = rs.getString("email");
             String password = rs.getString("password");
-            
+
             // encrypt the password using StrongPasswordEncryptor
             String encryptedPassword = passwordEncryptor.encryptPassword(password);
 
             // generate the update query
-            String updateQuery = String.format("UPDATE customers SET password='%s' WHERE id=%s;", encryptedPassword,
+            String updateQuery = String.format("UPDATE employees SET password='%s' WHERE email='%s';", encryptedPassword,
                     id);
+            System.out.println(updateQuery);
             updateQueryList.add(updateQuery);
         }
         rs.close();
--- src/main/java/VerifyPassword.java
+++ src/main/java/VerifyPassword.java
@@ -10,22 +10,24 @@ public class VerifyPassword {
 	/*
 	 * After you update the passwords in customers table,
 	 *   you can use this program as an example to verify the password.
-	 *   
+	 *
 	 * Verify the password is simple:
 	 * success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
-	 * 
+	 *
 	 * Note that you need to use the same StrongPasswordEncryptor when encrypting the passwords
-	 * 
+	 *
 	 */
 	public static void main(String[] args) throws Exception {
 
 		System.out.println(verifyCredentials("a@email.com", "a2"));
 		System.out.println(verifyCredentials("a@email.com", "a3"));
 
+		System.out.println(verifyCredentials("classta@email.edu", "classta"));
+
 	}
 
 	private static boolean verifyCredentials(String email, String password) throws Exception {
-		
+
 		String loginUser = "mytestuser";
 		String loginPasswd = "My6$Password";
 		String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
@@ -34,15 +36,15 @@ public class VerifyPassword {
 		Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
 		Statement statement = connection.createStatement();
 
-		String query = String.format("SELECT * from customers where email='%s'", email);
+		String query = String.format("SELECT * from employees where email='%s'", email);
 
 		ResultSet rs = statement.executeQuery(query);
 
 		boolean success = false;
 		if (rs.next()) {
-		    // get the encrypted password from the database
+			// get the encrypted password from the database
 			String encryptedPassword = rs.getString("password");
-			
+
 			// use the same encryptor to compare the user input password with encrypted password stored in DB
 			success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
 		}
@@ -50,7 +52,7 @@ public class VerifyPassword {
 		rs.close();
 		statement.close();
 		connection.close();
-		
+
 		System.out.println("verify " + email + " - " + password);
 
 		return success;
